7a7c498b4fef901f1017632e22c98881e4122186
Sébastien Richard <sebastien.r.richard@atos.net>
2020-04-18 00:53:45 +0200

    avancement des taches de remove, copy , unzip dans serveur ok

--- tkintertutos/sicsinstaller/server/model/Configuration.py
+++
@@ -1,8 +0,0 @@
-class Configuration:
-    def __init__(self):
-
-        self.backUpFile ="backUp.bin"
-
-        #default if not backup found
-        self.vals={ "sourceDir":"sourceDir", "macsHierarchy":"macsHierarchy", "sicsHierarchy":"sicsHierarchy", "macsLabel":"ABC", "macsComPort":"COM4", "macsChannelsConf":"1,2"}
-
